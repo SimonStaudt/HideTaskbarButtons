fd613351f14c37c7e63ea721aa122c3f01abfeef
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 14:26:48 +0200

    Added functionality to change visibility of Primary Taskbar, Start Menu, Search, Cortana, Contact, Taskview and News Buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HideTaskbarButtons;
+using static WinDlls;
 
 namespace HideTaskbarButtons
 {
@@ -20,9 +22,162 @@ namespace HideTaskbarButtons
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum TaskbarButtonsAndAreas
+        {
+            StartBtn,
+            SearchBtn,
+            CortanaBtn,
+            TaskviewBtn,
+            ContactsBtn,
+            NewsBtn,
+            TrayBtn,
+
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            //Title = Application.Current.MainWindow.GetType().Assembly.ReflectionOnly.ToString(); // Sets the Title of this Window to the Project (Assembly) Name
+            //Title = Application.ProductName;
+            Title = Application.ResourceAssembly.GetName().Name;
+            UpdateContentTextOf_btnShowHidePrimaryTaskbar(); // Updates the button to say the opposite of the visibility of the primary taskbar (e.g. if primary taskbar visible - button text Hide Primary Taskbar)
+        }
+
+
+        private void btnShowHidePrimaryTaskbar_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr taskbarWindow = FindWindow("Shell_TrayWnd", "");
+
+            if (IsWindowVisible(taskbarWindow))
+            {
+                ShowWindow(taskbarWindow, 0);
+                btnShowHidePrimaryTaskbar.Content = "Show Primary Taskbar";
+            }
+            else
+            {
+                ShowWindow(taskbarWindow, 1);
+                btnShowHidePrimaryTaskbar.Content = "Hide Primary Taskbar";
+            }
+        }
+
+        void UpdateContentTextOf_btnShowHidePrimaryTaskbar()
+        {
+            IntPtr taskbarWindow = FindWindow("Shell_TrayWnd", "");
+
+            if (!IsWindowVisible(taskbarWindow))
+            {
+                btnShowHidePrimaryTaskbar.Content = "Show Primary Taskbar";
+            }
+            else
+            {
+                btnShowHidePrimaryTaskbar.Content = "Hide Primary Taskbar";
+            }
+        }
+
+        private void ShowPrimaryStartBtn(object sender, RoutedEventArgs e)
+        {
+            IntPtr primarytaskbarWindow = FindWindow("Shell_TrayWnd", "");
+            IntPtr startBtn = FindWindowEx(primarytaskbarWindow, IntPtr.Zero, "Start", "Start");
+            ShowWindow(startBtn, 0);
+        }
+        private void HidePrimaryStartBtn(object sender, RoutedEventArgs e)
+        {
+            IntPtr primarytaskbarWindow = FindWindow("Shell_TrayWnd", "");
+            IntPtr startBtn = FindWindowEx(primarytaskbarWindow, IntPtr.Zero, "Start", "Start");
+            ShowWindow(startBtn, 1);
+        }
+
+
+        void ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas taskbarButtonsAndAreasToShowHide, int taskbar)
+        {
+            IntPtr taskbarWindow;
+            if (taskbar == 0)
+            {
+                taskbarWindow = FindWindow("Shell_TrayWnd", "");
+            }
+            else
+            {
+                taskbarWindow = FindWindow("Shell_SecondTrayWnd", "");
+            }
+
+            IntPtr startBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "Start", "Start");
+            IntPtr searchBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Zur Suche Text hier eingeben");
+            IntPtr cortanaBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Mit Cortana sprechen");
+            IntPtr taskviewBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Aktive Anwendungen");
+            IntPtr contactsBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Kontakte");
+            IntPtr newsBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Kontakte");
+
+            if (contactsBtn == IntPtr.Zero)
+            {
+                contactsBtn = FindWindowEx(taskbarWindow, IntPtr.Zero, "TrayButton", "Contacts");
+            }
+
+            switch (taskbarButtonsAndAreasToShowHide)
+            {
+                case TaskbarButtonsAndAreas.StartBtn:
+                    ShowHideWindowDependingOnVisibility(startBtn);
+                    break;
+                case TaskbarButtonsAndAreas.SearchBtn:
+                    ShowHideWindowDependingOnVisibility(searchBtn);
+                    break;
+                case TaskbarButtonsAndAreas.CortanaBtn:
+                    ShowHideWindowDependingOnVisibility(cortanaBtn);
+                    break;
+                case TaskbarButtonsAndAreas.TaskviewBtn:
+                    ShowHideWindowDependingOnVisibility(taskviewBtn);
+                    break;
+                case TaskbarButtonsAndAreas.ContactsBtn:
+                    ShowHideWindowDependingOnVisibility(contactsBtn);
+                    break;
+                case TaskbarButtonsAndAreas.NewsBtn:
+                    ShowHideWindowDependingOnVisibility(newsBtn);
+                    break;
+                case TaskbarButtonsAndAreas.TrayBtn:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ShowHideWindowDependingOnVisibility(IntPtr intPtr)
+        {
+            if (IsWindowVisible(intPtr)) ShowWindow(intPtr, 0); else ShowWindow(intPtr, 1);
+        }
+
+        private void SearchBtn(object sender, RoutedEventArgs e)
+        {
+            ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas.SearchBtn, Convert.ToInt32(((CheckBox)sender).Tag));
+            //Show Hides the taskbar button of the primary secondary or third ... (third ... not implemented)
+            //according to the tag of the checkbox
+        }
+
+
+        private void CortanaBtn(object sender, RoutedEventArgs e)
+        {
+            ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas.CortanaBtn, Convert.ToInt32(((CheckBox)sender).Tag));
+            //Show Hides the taskbar button of the primary secondary or third ... (third ... not implemented)
+            //according to the tag of the checkbox
+        }
+
+        private void TaskviewBtn(object sender, RoutedEventArgs e)
+        {
+            ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas.TaskviewBtn, Convert.ToInt32(((CheckBox)sender).Tag));
+        }
+
+        private void ContactsBtn(object sender, RoutedEventArgs e)
+        {
+            ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas.ContactsBtn, Convert.ToInt32(((CheckBox)sender).Tag));
+        }
+
+        private void NewsBtn(object sender, RoutedEventArgs e)
+        {
+            ShowHideTaskbarButtonsAndAreas(TaskbarButtonsAndAreas.NewsBtn, Convert.ToInt32(((CheckBox)sender).Tag));
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Author: Simon Staudt \nGithub: https://github.com/SimonStaudt/HideTaskbarButtons", Title + " - About");
         }
     }
 }
---
+++ WinDlls.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class WinDlls
+{
+    #region findwindow
+    /// <summary>
+    ///     Retrieves a handle to the top-level window whose class name and window name match the specified strings. This
+    ///     function does not search child windows. This function does not perform a case-sensitive search. To search child
+    ///     windows, beginning with a specified child window, use the
+    ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms633500%28v=vs.85%29.aspx">FindWindowEx</see>
+    ///     function.
+    ///     <para>
+    ///     Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633499%28v=vs.85%29.aspx for FindWindow
+    ///     information or https://msdn.microsoft.com/en-us/library/windows/desktop/ms633500%28v=vs.85%29.aspx for
+    ///     FindWindowEx
+    ///     </para>
+    /// </summary>
+    /// <param name="lpClassName">
+    ///     C++ ( lpClassName [in, optional]. Type: LPCTSTR )<br />The class name or a class atom created by a previous call to
+    ///     the RegisterClass or RegisterClassEx function. The atom must be in the low-order word of lpClassName; the
+    ///     high-order word must be zero.
+    ///     <para>
+    ///     If lpClassName points to a string, it specifies the window class name. The class name can be any name
+    ///     registered with RegisterClass or RegisterClassEx, or any of the predefined control-class names.
+    ///     </para>
+    ///     <para>If lpClassName is NULL, it finds any window whose title matches the lpWindowName parameter.</para>
+    /// </param>
+    /// <param name="lpWindowName">
+    ///     C++ ( lpWindowName [in, optional]. Type: LPCTSTR )<br />The window name (the window's
+    ///     title). If this parameter is NULL, all window names match.
+    /// </param>
+    /// <returns>
+    ///     C++ ( Type: HWND )<br />If the function succeeds, the return value is a handle to the window that has the
+    ///     specified class name and window name. If the function fails, the return value is NULL.
+    ///     <para>To get extended error information, call GetLastError.</para>
+    /// </returns>
+    /// <remarks>
+    ///     If the lpWindowName parameter is not NULL, FindWindow calls the <see cref="M:GetWindowText" /> function to
+    ///     retrieve the window name for comparison. For a description of a potential problem that can arise, see the Remarks
+    ///     for <see cref="M:GetWindowText" />.
+    /// </remarks>
+    // For Windows Mobile, replace user32.dll with coredll.dll
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    // Find window by Caption only. Note you must pass IntPtr.Zero as the first parameter.
+
+    [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+    public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+    // You can also call FindWindow(default(string), lpWindowName) or FindWindow((string)null, lpWindowName)
+    #endregion
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hWndChildAfter, string className, string windowTitle);
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool IsWindowVisible(IntPtr hWnd);
+}
